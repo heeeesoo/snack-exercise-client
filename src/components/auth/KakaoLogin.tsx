38a7d7010239c75a2b51e8deeb470292861d3003
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-07-10 12:54:18 +0900

    feat : env 파일에 kakao 키 추가

--- src/components/auth/KakaoLogin.tsx
+++ src/components/auth/KakaoLogin.tsx
@@ -3,12 +3,13 @@ import { useRouter } from 'next/navigation';
 
 const KakaoLogin = () => {
     const router = useRouter();
-    const REST_APIK_EY=process.env.KAKAO_CLIENT_ID;
-    const KAKAO_REDIRECT_URL=process.env.KAKAO_REDIRECT_URL;
-    const kakaoURL=`https://kauth.kakao.com/oauth/authorize?client_id=${REST_APIK_EY}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`;
+    const REST_API_KEY=process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+    const KAKAO_REDIRECT_URL=process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL;
+    const kakaoURL=`https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`;
 
     const handleClick = () => {
         router.push(kakaoURL);
+        console.log(kakaoURL);
     }
 
     return (
