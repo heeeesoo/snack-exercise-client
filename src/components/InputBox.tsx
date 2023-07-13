24e61f9b6119c0f0b9af584789365fa17fad4332
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-07-13 15:22:12 +0900

    style : theme color 추가 #14

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -56,4 +56,80 @@ export function SelectBox({
             />
         </div>
     );
+
+}
+
+export function SelectInputBox({ 
+    type, 
+    id, 
+    name, 
+    title,
+    subtitle,
+    placeholder
+ } : InputProps) {
+    return (
+        <div className="flex flex-col w-4/5 text-[16px]">
+            <div className="flex ">
+                <label htmlFor={name}>{title}</label>
+                <div>{subtitle}</div>
+            </div>
+            <input 
+                type={type} 
+                id={id} 
+                name={name} 
+                className="rounded-xl h-[60px] mt-[10px] pl-[14px]"
+                placeholder={placeholder}
+            />
+        </div>
+    );
+}
+
+export function RadioSelectBox({ 
+    type, 
+    id, 
+    name, 
+    title,
+    subtitle,
+    placeholder
+ } : InputProps) {
+    return (
+        <div className="flex flex-col w-4/5 text-[16px]">
+            <div className="flex ">
+                <label htmlFor={name}>{title}</label>
+                <div>{subtitle}</div>
+            </div>
+            <input 
+                type={type} 
+                id={id} 
+                name={name} 
+                className="rounded-xl h-[60px] mt-[10px] pl-[14px]"
+                placeholder={placeholder}
+            />
+        </div>
+    );
+}
+
+export function SearchBox({ 
+    type, 
+    id, 
+    name, 
+    title,
+    subtitle,
+    placeholder
+ } : InputProps) {
+    return (
+        <div className="flex flex-col w-4/5 text-[16px]">
+            <div className="flex ">
+                <label htmlFor={name}>{title}</label>
+                <div>{subtitle}</div>
+            </div>
+            <input 
+                type={type} 
+                id={id} 
+                name={name} 
+                className="rounded-xl h-[60px] mt-[10px] pl-[14px]"
+                placeholder={placeholder}
+            />
+        </div>
+    );
 }
