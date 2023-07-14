bd4b1a92f7a923d3eebae346a3e2c0f6426bb121
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-07-14 17:06:32 +0900

    feat : fetchLogin 함수 추가 #20

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
     return (
         <div className="flex items-center justify-center h-[60px] w-full bg-grayScreen">
-            <Image
-            src="/logo/logo_small.svg"
-            width={108}
-            height={24}
-            alt="logo"
-            />
+            <Link href="/">
+                <Image
+                src="/logo/logo_small.svg"
+                width={108}
+                height={24}
+                alt="logo"
+                />
+            </Link>
         </div>
     );
 };
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,5 +1,6 @@
 export const NavLinks = [
-    { href: '/dashboard', key: 'exercise', text: '운동' },
-    { href: '/', key: 'home', text: 'Home' },
-    { href: '/', key: 'statistics', text: '통계' },
+    { href: '/', key: 'home', text: '홈', img: '/icons/footer/home.svg' },
+    { href: '/my/exercise', key: 'exercise', text: '나의 운동', img: '/icons/footer/exercise.svg' },
+    { href: '/calendar', key: 'calendar', text: '통계', img: '/icons/footer/calendar.svg' },
+    { href: '/my', key: 'my', text: '마이', img: '/icons/footer/my.svg' },
 ];
