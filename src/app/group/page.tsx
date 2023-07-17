7f97188d72c327694a3620c0b6b45b29e36398ed
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-07-18 08:12:34 +0900

    feat : 그룹 만들기 routing 추가 #26

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -3,13 +3,18 @@
 import MissionCard from "@/components/card/MissionCard";
 import { IconVerticalButton } from "@/components/common/Button";
 import GuideSwiper from "@/components/card/GuideSwiper";
+import { useRouter } from 'next/navigation';
 
 import { People } from "@/constant/icon";
 import { Mail } from "@/constant/icon";
 
 const page = () => {
+    const router = useRouter();
     const handleClick = () => {
         console.log('!')
+    }
+    const handleGruopClick = () => {
+        router.push('/group/create');
     }
     return (
         <div className="flex flex-col items-center">
@@ -24,7 +29,7 @@ const page = () => {
             </div>
             <div className="pb-[40px]"></div>
             <div className="flex justify-between w-9xl">
-                <IconVerticalButton title="그룹 만들기" onClick={handleClick} imglink={People}/>
+                <IconVerticalButton title="그룹 만들기" onClick={handleGruopClick} imglink={People}/>
                 <IconVerticalButton title="초대 코드" onClick={handleClick} imglink={Mail}/>
             </div>
             <div className="text-[20px] font-bold w-9xl pb-[16px] pt-[40px]">
