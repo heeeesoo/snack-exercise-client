1e821b92e33915f4e55f0570a2b0b29ece6dc12a
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-07-21 10:47:31 +0900

    chore : component 대문자로 변경 #41

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,4 +1,4 @@
-const page = () => {
+const Page = () => {
     return (
         <div>
             calendar
@@ -6,4 +6,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default Page;
