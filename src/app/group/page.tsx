d25ea5cf75d79d85d572de03738f103542e45cbe
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-07-30 07:58:48 +0900

    feat : group navbar scroll 삭제 #50

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -26,6 +26,7 @@ export default function Group() {
     // html 매칭 문제 해결
     useEffect(() => {
         setMounted(true);
+        setId(arrGroup[0]?.id);
     }, []);
 
 
@@ -52,11 +53,11 @@ export default function Group() {
         <div className="flex flex-col items-center">
             
             {/* <GroupNav handleIdChange={handleIdChange}/> */}
-            <div className="flex flex-row overflow-auto w-screen max-w-[400px] h-auto">
+            <div className="flex flex-row overflow-auto w-screen max-w-[400px] h-auto ml-[20px] no-scrollbar">
                 <section className="flex flex-row h-[36px]">
                     {arrGroup.map((group : any) => {
                         return (
-                            <div key={group.id} className="text-white bg-[#212131] mr-[8px] rounded-[16px] w-[88px] h-[36px] flex items-center justify-center">
+                            <div key={group.id} className={`${group.id === id ? 'bg-SystemDarkBlue text-white' : 'text-SystemGray9'} mr-[8px] rounded-[16px] w-[88px] h-[36px] flex items-center justify-center`}>
                                 <button onClick={() => handleIdChange(group.id)}>{group.name}</button>
                             </div>
                         )
