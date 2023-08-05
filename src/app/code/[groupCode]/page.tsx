c5a93e9d5cd81bc8e814847f8245858575626cee
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-08-06 00:23:09 +0900

    feat : 초대코드 복사 기능 추가 #58

--- src/app/code/[groupCode]/page.tsx
+++ src/app/code/[groupCode]/page.tsx
@@ -1,10 +1,15 @@
 'use client'
+import CopyInviteCodeButton from "@/components/common/CopyInviteCodeButton"
 
 export default function GroupCode({ params }: { params: { groupCode: string } }) {
 
   return (
-    <div className="flex flex-col items-center justify-center">
-        {params.groupCode}
+    <div className="flex flex-col items-center justify-center h-[80vh]">
+        <div className="font-bold text-[40px]">
+            {params.groupCode}  
+        </div>
+        <div className="py-[10px]"/>
+        <CopyInviteCodeButton inviteCode={params.groupCode} />
     </div>
   )
 }
