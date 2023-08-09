84f3897c81edbe639bd6e9964cd9243616b6fb42
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-08-09 18:41:50 +0900

    chore : default emozi 변경 #69

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -38,11 +38,16 @@ const Page = () => {
                     groupFinishList.map((group : GroupFinishType) => {
                         return(
                             <div key={group.groupId} className="bg-white w-9xl h-[60px] rounded-[16px] mb-[5px] flex justify-between items-center px-[15px]">
-                                <div className="">
+                                <div className="font-bold">
                                     {group.groupName}
                                 </div>
-                                <div className="w-[30px] bg-SystemBrand text-white">
-                                    {group.isGoalAchieved ? '성공' : '실패'}
+                                <div className="flex flex-row items-center justify-center">
+                                    <div className="pr-[10px] text-[12px] text-SystemGray3">
+                                        릴레이 총 {group.finishedRelayNum-1} / {group.goalRelayNum}회
+                                    </div>
+                                    <div className="w-[50px] bg-SystemBrand text-white rounded-md flex items-center justify-center">
+                                        {group.isGoalAchieved ? '성공' : '실패'}
+                                    </div>
                                 </div>
                             </div>
                         )
